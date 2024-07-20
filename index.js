03707b19c2c34d68e4121bf619c93d6343393cb3
Anjila <gurunganjila27@gmail.com>
2024-07-20 15:26:10 +0545

    updated readme

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = Number(process.env.PORT)||8000;
+const PORT = Number(process.env.PORT) || 8000;
 const indexRouter = require("./routes");
 
-mongoose.connect (process.env.DB_URl).then (()=>{
-    console.log("Database is connected...");
+mongoose.connect(process.env.DB_URl).then(() => {
+  console.log("Database is connected...");
 });
 
+app.use(cors);
 app.use(express.json());
-app.use("/",indexRouter);
+app.use("/", indexRouter);
 
-app.use((err,req,res,next)=>{
-    const errMsg = err.toString()|| "something went wrong";
-    res.status(500).json({msg:errMsg});
+app.use((err, req, res, next) => {
+  const errMsg = err.toString() || "something went wrong";
+  res.status(500).json({ msg: errMsg });
 });
-app.listen(PORT,()=>{
-    console.log(`Application is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}`);
 });
-
